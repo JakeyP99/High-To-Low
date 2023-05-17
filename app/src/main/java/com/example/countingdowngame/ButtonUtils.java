152b49dbc800209c2141c262875aacdd5a0749bf
Jake Puglia <jpuglia99@hotmail.com>
2023-05-17 20:34:01 +0800

    Added in sound array to add in different effects.

--- app/src/main/java/com/example/countingdowngame/ButtonUtils.java
+++ app/src/main/java/com/example/countingdowngame/ButtonUtils.java
@@ -3,6 +3,7 @@ package com.example.countingdowngame;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.os.Build;
@@ -12,13 +13,26 @@ import android.view.MotionEvent;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+
+import java.io.IOException;
 
 public class ButtonUtils {
 
-    public static void setButton(final Button button, final Class<?> activityClass, final AppCompatActivity context, final Runnable buttonAction) {
+    private static final int NUM_SOUNDS = 4; // Number of sounds in the rotation
+    private static int currentSoundIndex = 0; // Current index of the sound being played
+    private static MediaPlayer[] burp = new MediaPlayer[NUM_SOUNDS];
+
+    public static void setButton(final Button button, final Class<?> activityClass, final Context context, final Runnable buttonAction) {
+        burp[0] = MediaPlayer.create(context, R.raw.burp1);
+        burp[1] = MediaPlayer.create(context, R.raw.burp2);
+        burp[2] = MediaPlayer.create(context, R.raw.burp3);
+        burp[3] = MediaPlayer.create(context, R.raw.burp4);
+
         final MediaPlayer bop = MediaPlayer.create(context, R.raw.bop);
+
+        SharedPreferences preferences = context.getSharedPreferences("sound_mode_choice", Context.MODE_PRIVATE);
+        boolean soundEffects = preferences.getBoolean("button_regularSound", false);
 
         button.setOnTouchListener((view, motionEvent) -> {
             if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
@@ -28,29 +42,51 @@ public class ButtonUtils {
                 if (activityClass != null) {
                     Intent intent = new Intent(context, activityClass);
                     context.startActivity(intent);
-//                context.finish(); // Check if this line is needed
                 }
                 if (buttonAction != null) {
                     buttonAction.run();
                 }
                 vibrateDevice(context);
-                bop.start();
 
+                if (soundEffects) {
+                    // Play the bop sound effect
+                    bop.start();
+                } else {
+                    // Update the current sound index and handle overflow
+                    currentSoundIndex = (currentSoundIndex + 1) % NUM_SOUNDS;
+
+                    // Stop any previously playing sound
+                    try {
+                        stopCurrentSound();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    // Play the current sound
+                    burp[currentSoundIndex].start();
+                }
             }
             return true;
         });
     }
-       public static void setImageButton ( final ImageButton imagebutton, final Class<?> activityClass, final Context context, final Runnable buttonAction){
-            imagebutton.setOnTouchListener((view, motionEvent) -> {
-                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                    imagebutton.setBackground(context.getDrawable(R.drawable.buttonhighlight));
-                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                    imagebutton.setBackground(context.getDrawable(R.drawable.outlineforbutton));
-                    if (activityClass != null) {
-                        Intent intent = new Intent(context, activityClass);
-                        context.startActivity(intent);
-                    }
-                    if (buttonAction != null) {
+
+    private static void stopCurrentSound() throws IOException {
+        if (burp[currentSoundIndex] != null) {
+            burp[currentSoundIndex].stop();
+            burp[currentSoundIndex].prepare();
+        }
+    }
+    public static void setImageButton(final ImageButton imagebutton, final Class<?> activityClass, final Context context, final Runnable buttonAction) {
+        imagebutton.setOnTouchListener((view, motionEvent) -> {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                imagebutton.setBackground(context.getDrawable(R.drawable.buttonhighlight));
+            } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                imagebutton.setBackground(context.getDrawable(R.drawable.outlineforbutton));
+                if (activityClass != null) {
+                    Intent intent = new Intent(context, activityClass);
+                    context.startActivity(intent);
+                }
+                if (buttonAction != null) {
                         buttonAction.run();
                     }
                     vibrateDevice(context);
--- app/src/main/java/com/example/countingdowngame/Settings_GameModeChoice.java
+++ app/src/main/java/com/example/countingdowngame/Settings_GameModeChoice.java
@@ -3,7 +3,6 @@ package com.example.countingdowngame;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,16 +11,17 @@ public class Settings_GameModeChoice extends AppCompatActivity {
 
     private Button button_gameModeOne;
     private Button button_gameModeTwo;
+    private Button button_regularSound;
+    private Button button_burpSound;
+
+    Button btnReturn;
+
+
     private Drawable buttonHighlightDrawable;
 
     @Override
     public void onBackPressed() {
-        SharedPreferences preferences = getSharedPreferences("game_mode_choice", MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putBoolean("button_gameModeOne", button_gameModeOne.isSelected());
-        editor.putBoolean("button_gameModeTwo", button_gameModeTwo.isSelected());
-        editor.apply();
-
+        savePreferences();
         super.onBackPressed();
     }
 
@@ -32,17 +32,25 @@ public class Settings_GameModeChoice extends AppCompatActivity {
 
         button_gameModeOne = findViewById(R.id.button_gameModeOne);
         button_gameModeTwo = findViewById(R.id.button_gameModeTwo);
+        button_regularSound = findViewById(R.id.button_normal_sound);
+        button_burpSound = findViewById(R.id.button_burp_sound);
         buttonHighlightDrawable = getResources().getDrawable(R.drawable.buttonhighlight);
+        btnReturn = findViewById(R.id.buttonReturn);
 
-        // Retrieve the saved state of the buttons from shared preferences
         SharedPreferences preferences = getSharedPreferences("game_mode_choice", MODE_PRIVATE);
-        boolean buttonOneSelected = preferences.getBoolean("button_gameModeOne", true); // Set default value to true
+        boolean buttonOneSelected = preferences.getBoolean("button_gameModeOne", true);
 
-        // Set the buttons accordingly
         button_gameModeOne.setSelected(buttonOneSelected);
-        button_gameModeTwo.setSelected(!buttonOneSelected); // Invert the selection for button_gameModeTwo
+        button_gameModeTwo.setSelected(!buttonOneSelected);
 
-        // Apply button highlight drawable if buttons are selected
+
+        ButtonUtils.setButton(btnReturn, HomeScreen.class, this, () ->
+        {
+            savePreferences();
+            super.onBackPressed();
+        });
+
+
         if (buttonOneSelected) {
             button_gameModeOne.setBackground(buttonHighlightDrawable);
             button_gameModeTwo.setBackground(null);
@@ -51,39 +59,53 @@ public class Settings_GameModeChoice extends AppCompatActivity {
             button_gameModeTwo.setBackground(buttonHighlightDrawable);
         }
 
-        // Add listeners to the buttons
-        button_gameModeOne.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                boolean isSelected = !button_gameModeOne.isSelected();
-                button_gameModeOne.setSelected(isSelected);
-                button_gameModeTwo.setSelected(!isSelected);
+        button_gameModeOne.setOnClickListener(v -> toggleButton(button_gameModeOne, button_gameModeTwo));
+        button_gameModeTwo.setOnClickListener(v -> toggleButton(button_gameModeTwo, button_gameModeOne));
 
-                if (isSelected) {
-                    button_gameModeOne.setBackground(buttonHighlightDrawable);
-                    button_gameModeTwo.setBackground(null);
-                } else {
-                    button_gameModeOne.setBackground(null);
-                    button_gameModeTwo.setBackground(buttonHighlightDrawable);
-                }
-            }
-        });
+        SharedPreferences soundPreferences = getSharedPreferences("sound_mode_choice", MODE_PRIVATE);
+        boolean regularSoundSelected = soundPreferences.getBoolean("button_regularSound", true);
 
-        button_gameModeTwo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                boolean isSelected = !button_gameModeTwo.isSelected();
-                button_gameModeTwo.setSelected(isSelected);
-                button_gameModeOne.setSelected(!isSelected);
+        button_regularSound.setSelected(regularSoundSelected);
+        button_burpSound.setSelected(!regularSoundSelected);
 
-                if (isSelected) {
-                    button_gameModeTwo.setBackground(buttonHighlightDrawable);
-                    button_gameModeOne.setBackground(null);
-                } else {
-                    button_gameModeTwo.setBackground(null);
-                    button_gameModeOne.setBackground(buttonHighlightDrawable);
-                }
-            }
-        });
+        if (regularSoundSelected) {
+            button_regularSound.setBackground(buttonHighlightDrawable);
+            button_burpSound.setBackground(null);
+        } else {
+            button_regularSound.setBackground(null);
+            button_burpSound.setBackground(buttonHighlightDrawable);
+        }
+
+        button_regularSound.setOnClickListener(v -> toggleButton(button_regularSound, button_burpSound));
+        button_burpSound.setOnClickListener(v -> toggleButton(button_burpSound, button_regularSound));
+    }
+
+    private void toggleButton(Button selectedButton, Button unselectedButton) {
+        boolean isSelected = !selectedButton.isSelected();
+        selectedButton.setSelected(isSelected);
+        unselectedButton.setSelected(!isSelected);
+
+        if (isSelected) {
+            selectedButton.setBackground(buttonHighlightDrawable);
+            unselectedButton.setBackground(null);
+        } else {
+            selectedButton.setBackground(null);
+            unselectedButton.setBackground(buttonHighlightDrawable);
+        }
+    }
+
+    private void savePreferences() {
+        SharedPreferences gameModePreferences = getSharedPreferences("game_mode_choice", MODE_PRIVATE);
+        SharedPreferences.Editor gameModeEditor = gameModePreferences.edit();
+
+        gameModeEditor.putBoolean("button_gameModeOne", button_gameModeOne.isSelected());
+        gameModeEditor.putBoolean("button_gameModeTwo", button_gameModeTwo.isSelected());
+        gameModeEditor.apply();
+
+        SharedPreferences soundPreferences = getSharedPreferences("sound_mode_choice", MODE_PRIVATE);
+        SharedPreferences.Editor soundEditor = soundPreferences.edit();
+
+        soundEditor.putBoolean("button_regularSound", button_regularSound.isSelected());
+        soundEditor.apply();
     }
 }
