6d5d887fc2c0ba098b1893fdb8e942a39b6fdd6d
Jake Puglia <jpuglia99@hotmail.com>
2023-06-20 15:29:40 +0800

    Fragments are not saving and loading

--- app/src/main/java/com/example/countingdowngame/QuizWildCardsAdapter.java
+++ app/src/main/java/com/example/countingdowngame/QuizWildCardsAdapter.java
@@ -1,11 +1,21 @@
 package com.example.countingdowngame;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.text.Html;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class QuizWildCardsAdapter extends WildCardsAdapter {
     public QuizWildCardsAdapter(Settings_WildCard_Probabilities[] taskWildCards, Context context, Settings_WildCard_Mode mode) {
@@ -41,9 +51,101 @@ public class QuizWildCardsAdapter extends WildCardsAdapter {
         public void bind(Settings_WildCard_Probabilities wildcard) {
             textViewTitle.setText(wildcard.getText());
             textViewProbabilities.setText(String.valueOf(wildcard.getProbability()));
+            switchEnabled.setChecked(wildcard.isEnabled());
+
+            switchEnabled.setOnCheckedChangeListener((buttonView, isChecked) -> {
+                wildcard.setEnabled(isChecked);
+                saveWildCardProbabilitiesToStorage(mMode, wildCards);
+            });
+
+            editButton.setOnClickListener(v -> {
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                builder.setTitle("Edit Wildcard Properties");
+                int blueDarkColor = mContext.getResources().getColor(R.color.bluedark);
+
+                LinearLayout layout = new LinearLayout(mContext);
+                layout.setOrientation(LinearLayout.VERTICAL);
+
+                final EditText textInput = new EditText(mContext);
+
+                if (wildcard.isDeletable()) {
+                    textInput.setInputType(InputType.TYPE_CLASS_TEXT);
+                    textInput.setText(wildcard.getText());
+                    layout.addView(textInput);
+                } else {
+                    textInput.setEnabled(false);
+                }
+
+                final EditText probabilityInput = new EditText(mContext);
+                probabilityInput.setInputType(InputType.TYPE_CLASS_NUMBER);
+                probabilityInput.setText(String.valueOf(wildcard.getProbability()));
+                layout.addView(probabilityInput);
+
+                builder.setView(layout);
+
+                builder.setNegativeButton(Html.fromHtml("<font color='" + blueDarkColor + "'>Cancel</font>"), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                ArrayList<Settings_WildCard_Probabilities> wildCardList = new ArrayList<>(Arrays.asList(wildCards));
+
+                if (wildcard.isDeletable()) {
+                    builder.setNeutralButton(Html.fromHtml("<font color='" + blueDarkColor + "'>Delete</font>"), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            wildCardList.remove(getAdapterPosition());
+                            wildCards = wildCardList.toArray(new Settings_WildCard_Probabilities[0]);
+                            notifyDataSetChanged();
+                            saveWildCardProbabilitiesToStorage(mMode, wildCards);
+                        }
+                    });
+                }
+
+                builder.setPositiveButton(Html.fromHtml("<font color='" + blueDarkColor + "'>OK</font>"), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String inputText = textInput.getText().toString().trim();
+                        if (wildcard.isDeletable() && inputText.isEmpty()) {
+                            Toast.makeText(mContext, "The wildcard needs some text, please and thanks!", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        int probability;
+                        try {
+                            probability = Integer.parseInt(probabilityInput.getText().toString());
+                        } catch (NumberFormatException e) {
+                            probability = 0; // Invalid input, set to 0
+                        }
+
+                        if (probabilityInput.getText().length() > 4) {
+                            Toast.makeText(mContext, "Please enter a probability with 4 or fewer digits.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        if (textInput.length() > 100) {
+                            Toast.makeText(mContext, "Sorry, way too big of a wildcard boss man, limited to 100 characters.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        wildcard.setProbability(probability);
+
+                        if (wildcard.isDeletable()) {
+                            wildcard.setText(inputText);
+                            textViewTitle.setText(wildcard.getText());
+                        }
+
+                        textViewProbabilities.setText(String.valueOf(wildcard.getProbability()));
+                        setProbabilitySizeBasedOnString(textViewProbabilities, String.valueOf(wildcard.getProbability()));
+
+                        saveWildCardProbabilitiesToStorage(mMode, wildCards);
+                    }
+                });
+
+                builder.show();
+            });
         }
-
     }
-
-    // Additional TaskWildCardsAdapter specific methods and functionality here
 }
--- app/src/main/java/com/example/countingdowngame/WildCardsAdapter.java
+++ app/src/main/java/com/example/countingdowngame/WildCardsAdapter.java
@@ -1,14 +1,25 @@
 package com.example.countingdowngame;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.text.Html;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public abstract class WildCardsAdapter extends RecyclerView.Adapter<WildCardsAdapter.WildCardViewHolder> {
     protected Settings_WildCard_Probabilities[] wildCards;
@@ -21,6 +32,68 @@ public abstract class WildCardsAdapter extends RecyclerView.Adapter<WildCardsAda
         this.mMode = mode;
         loadWildCardProbabilitiesFromStorage(mode);
     }
+
+    private Settings_WildCard_Probabilities[] loadWildCardProbabilitiesFromStorage(Settings_WildCard_Mode mode) {
+        SharedPreferences prefs;
+        switch (mode) {
+            case DELETABLE:
+                prefs = mContext.getSharedPreferences("DeletablePrefs", MODE_PRIVATE);
+                break;
+            default:
+                // Handle other modes if necessary
+                return new Settings_WildCard_Probabilities[0];
+        }
+
+        int wildCardCount = prefs.getInt("wild_card_count", 0);
+        Settings_WildCard_Probabilities[] loadedWildCards = new Settings_WildCard_Probabilities[wildCardCount];
+
+        for (int i = 0; i < wildCardCount; i++) {
+            boolean enabled = prefs.getBoolean("wild_card_enabled_" + i, false);
+            String activity = prefs.getString("wild_card_activity_" + i, "");
+            int probability = prefs.getInt("wild_card_probability_" + i, 0);
+
+            loadedWildCards[i] = new Settings_WildCard_Probabilities(activity, probability, enabled, true);
+        }
+        return loadedWildCards;
+    }
+
+
+    void setProbabilitySizeBasedOnString(TextView textView, String text) {
+        int textSize = 18;
+        if (text.length() > 2) {
+            textSize = 12;
+        } else if (text.length() > 0) {
+            textSize = 18;
+        }
+        textView.setTextSize(textSize);
+    }
+
+    public void saveWildCardProbabilitiesToStorage(Settings_WildCard_Mode mode, Settings_WildCard_Probabilities[] probabilities) {
+        SharedPreferences prefs;
+        switch (mode) {
+            case DELETABLE:
+                prefs = mContext.getSharedPreferences("DeletablePrefs", MODE_PRIVATE);
+                break;
+            default:
+                // Handle other modes if necessary
+                return;
+        }
+
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.clear();
+        editor.putInt("wild_card_count", probabilities.length);
+
+        for (int i = 0; i < probabilities.length; i++) {
+            Settings_WildCard_Probabilities probability = probabilities[i];
+            editor.putBoolean("wild_card_enabled_" + i, probability.isEnabled());
+            editor.putString("wild_card_activity_" + i, probability.getText());
+            editor.putInt("wild_card_probability_" + i, probability.getProbability());
+        }
+
+        editor.apply();
+    }
+
 
     // Common methods and functionality here
 
@@ -38,45 +111,104 @@ public abstract class WildCardsAdapter extends RecyclerView.Adapter<WildCardsAda
             switchEnabled = itemView.findViewById(R.id.switch_wildcard);
         }
 
-        public abstract void bind(Settings_WildCard_Probabilities wildcard);
-    }
+        public void bind(Settings_WildCard_Probabilities wildcard) {
+            textViewTitle.setText(wildcard.getText());
+            textViewProbabilities.setText(String.valueOf(wildcard.getProbability()));
+            switchEnabled.setChecked(wildcard.isEnabled());
 
-    // Common methods and functionality here
+            switchEnabled.setOnCheckedChangeListener((buttonView, isChecked) -> {
+                wildcard.setEnabled(isChecked);
+                saveWildCardProbabilitiesToStorage(mMode, wildCards);
+            });
 
-    private Settings_WildCard_Probabilities[] loadWildCardProbabilitiesFromStorage(Settings_WildCard_Mode mode) {
-        // Common implementation
-        return new Settings_WildCard_Probabilities[0];
-    }
+            editButton.setOnClickListener(v -> {
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                builder.setTitle("Edit Wildcard Properties");
+                int blueDarkColor = mContext.getResources().getColor(R.color.bluedark);
 
-    private void saveWildCardProbabilitiesToStorage(Settings_WildCard_Mode mode, Settings_WildCard_Probabilities[] probabilities, Fragment targetFragment) {
-        SharedPreferences prefs;
-        String prefsName;
+                LinearLayout layout = new LinearLayout(mContext);
+                layout.setOrientation(LinearLayout.VERTICAL);
 
-        if (targetFragment instanceof TruthWildCardsFragment) {
-            prefsName = "TruthTaskPrefs";
-        } else if (targetFragment instanceof QuizWildCardsFragment) {
-            prefsName = "QuizWildCardsPrefs";
-        } else {
-            // Handle other fragments if necessary
-            return;
+                final EditText textInput = new EditText(mContext);
+
+                if (wildcard.isDeletable()) {
+                    textInput.setInputType(InputType.TYPE_CLASS_TEXT);
+                    textInput.setText(wildcard.getText());
+                    layout.addView(textInput);
+                } else {
+                    textInput.setEnabled(false);
+                }
+
+                final EditText probabilityInput = new EditText(mContext);
+                probabilityInput.setInputType(InputType.TYPE_CLASS_NUMBER);
+                probabilityInput.setText(String.valueOf(wildcard.getProbability()));
+                layout.addView(probabilityInput);
+
+                builder.setView(layout);
+
+                builder.setNegativeButton(Html.fromHtml("<font color='" + blueDarkColor + "'>Cancel</font>"), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                ArrayList<Settings_WildCard_Probabilities> wildCardList = new ArrayList<>(Arrays.asList(wildCards));
+
+                if (wildcard.isDeletable()) {
+                    builder.setNeutralButton(Html.fromHtml("<font color='" + blueDarkColor + "'>Delete</font>"), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            wildCardList.remove(getAdapterPosition());
+                            wildCards = wildCardList.toArray(new Settings_WildCard_Probabilities[0]);
+                            notifyDataSetChanged();
+                            saveWildCardProbabilitiesToStorage(mMode, wildCards);
+                        }
+                    });
+                }
+
+                builder.setPositiveButton(Html.fromHtml("<font color='" + blueDarkColor + "'>OK</font>"), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String inputText = textInput.getText().toString().trim();
+                        if (wildcard.isDeletable() && inputText.isEmpty()) {
+                            Toast.makeText(mContext, "The wildcard needs some text, please and thanks!", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        int probability;
+                        try {
+                            probability = Integer.parseInt(probabilityInput.getText().toString());
+                        } catch (NumberFormatException e) {
+                            probability = 0; // Invalid input, set to 0
+                        }
+
+                        if (probabilityInput.getText().length() > 4) {
+                            Toast.makeText(mContext, "Please enter a probability with 4 or fewer digits.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        if (textInput.length() > 100) {
+                            Toast.makeText(mContext, "Sorry, way too big of a wildcard boss man, limited to 100 characters.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        wildcard.setProbability(probability);
+
+                        if (wildcard.isDeletable()) {
+                            wildcard.setText(inputText);
+                            textViewTitle.setText(wildcard.getText());
+                        }
+
+                        textViewProbabilities.setText(String.valueOf(wildcard.getProbability()));
+                        setProbabilitySizeBasedOnString(textViewProbabilities, String.valueOf(wildcard.getProbability()));
+
+                        saveWildCardProbabilitiesToStorage(mMode, wildCards);
+                    }
+                });
+
+                builder.show();
+            });
         }
-
-        prefs = mContext.getSharedPreferences(prefsName, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-
-        editor.clear();
-        editor.putInt("wild_card_count", probabilities.length);
-
-        for (int i = 0; i < probabilities.length; i++) {
-            Settings_WildCard_Probabilities probability = probabilities[i];
-            editor.putBoolean("wild_card_enabled_" + i, probability.isEnabled());
-            editor.putString("wild_card_activity_" + i, probability.getText());
-            editor.putInt("wild_card_probability_" + i, probability.getProbability());
-        }
-
-        editor.apply();
     }
-
-
-
 }
