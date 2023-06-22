cbbab8de2ca86fcaa4bcc5d710400a0e81fc1f2b
Jake Puglia <jpuglia99@hotmail.com>
2023-06-22 16:20:17 +0800

    The error for wildcard quanitity is sorted, just need to fix the message

--- app/src/main/java/com/example/countingdowngame/Settings_WildCard_Settings.java
+++ app/src/main/java/com/example/countingdowngame/Settings_WildCard_Settings.java
@@ -3,6 +3,9 @@ package com.example.countingdowngame;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -11,7 +14,9 @@ public class Settings_WildCard_Settings extends ButtonUtilsActivity {
 
     //-----------------------------------------------------Initialize---------------------------------------------------//
     private Button btnReturn;
-    private static int loadWildCardAmount;
+    private EditText wildcardPerPlayerEditText;
+    private boolean isValidInput;
+
 
     //-----------------------------------------------------On Pause---------------------------------------------------//
 
@@ -37,41 +42,91 @@ public class Settings_WildCard_Settings extends ButtonUtilsActivity {
         buttonHighlightDrawable = getResources().getDrawable(R.drawable.buttonhighlight);
         outlineForButton = getResources().getDrawable(R.drawable.outlineforbutton);
         btnReturn = findViewById(R.id.buttonReturn);
+
+        wildcardPerPlayerEditText = findViewById(R.id.edittext_wildcard_amount);
+        wildcardPerPlayerEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // No action needed
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // No action needed
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                validateWildCardAmount();
+            }
+        });
     }
+
 
     private void setButtonListeners() {
         btnUtils.setButton(btnReturn, this::onBackPressed);
+    }
+
+    private void onReturnButtonClicked(View view) {
+        if (isValidInput) {
+            onBackPressed();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (isValidInput) {
+            savePreferences();
+            super.onBackPressed();
+        }
     }
     //-----------------------------------------------------Get WildCard---------------------------------------------------//
 
     public static int getWildCardAmountFromSettings(Context context) {
         SharedPreferences wildcardPreferences = context.getSharedPreferences("wildcard_amount", MODE_PRIVATE);
-        int amount = wildcardPreferences.getInt("wildcardAmount", 1);
-        return amount;
+        return wildcardPreferences.getInt("wildcardAmount", 1);
     }
+
+
+    private void validateWildCardAmount() {
+        String wildCardAmountInput = wildcardPerPlayerEditText.getText().toString().trim();
+
+       if (wildCardAmountInput.length() > 3) {
+            wildCardAmountInput = wildCardAmountInput.substring(0, 3);
+            wildcardPerPlayerEditText.setText(wildCardAmountInput);
+            wildcardPerPlayerEditText.setSelection(wildCardAmountInput.length());
+        }
+
+        int wildCardAmount;
+        try {
+            wildCardAmount = Integer.parseInt(wildCardAmountInput);
+            if (wildCardAmount > 100) {
+                wildcardPerPlayerEditText.setError("Please enter a number between 0 and 100");
+            } else {
+                wildcardPerPlayerEditText.setError(null);
+            }
+        } catch (NumberFormatException e) {
+            wildcardPerPlayerEditText.setError("Please enter a valid number");
+        }
+    }
+
+
+
 
     //-----------------------------------------------------Load and Save Preferences---------------------------------------------------//
 
     private void loadPreferences() {
-        // Load wildcard amount
         SharedPreferences wildcardPreferences = getSharedPreferences("wildcard_amount", MODE_PRIVATE);
-        loadWildCardAmount = wildcardPreferences.getInt("wildcardAmount", 1);
-        EditText wildcardPerPlayerEditText = findViewById(R.id.edittext_wildcard_amount);
+        int loadWildCardAmount = wildcardPreferences.getInt("wildcardAmount", 1);
         wildcardPerPlayerEditText.setText(String.valueOf(loadWildCardAmount));
-
     }
 
     private void savePreferences() {
-        // Save wildcard amount
-        EditText wildcardPerPlayerEditText = findViewById(R.id.edittext_wildcard_amount);
         int wildCardAmountSetInSettings = Integer.parseInt(wildcardPerPlayerEditText.getText().toString());
-
         SharedPreferences wildcardPreferences = getSharedPreferences("wildcard_amount", MODE_PRIVATE);
         SharedPreferences.Editor wildcardEditor = wildcardPreferences.edit();
         wildcardEditor.putInt("wildcardAmount", wildCardAmountSetInSettings);
         wildcardEditor.apply();
     }
-
-
 
 }
