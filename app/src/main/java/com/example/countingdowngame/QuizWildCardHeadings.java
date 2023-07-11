c4ce6314d6dc7f28199089ebeffb0621460e6ae7
Jake Puglia <jpuglia99@hotmail.com>
2023-07-11 19:51:42 +0800

    I have added a new headings to create an answer for my quiz wildcards, I also added the answer button

---
+++ app/src/main/java/com/example/countingdowngame/QuizWildCardHeadings.java
@@ -0,0 +1,18 @@
+package com.example.countingdowngame;
+
+public class QuizWildCardHeadings extends WildCardHeadings {
+    private String answer;
+
+    public QuizWildCardHeadings(String activity, int probability, boolean enabled, boolean deletable, String answer) {
+        super(activity, probability, enabled, deletable);
+        this.answer = answer;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
