135c3902193f6f4cd37d7bd79d676b32cfea0af3
Jake Puglia <jpuglia99@hotmail.com>
2023-10-08 20:24:33 +0800

    test

--- app/src/main/java/com/example/countingdowngame/mainActivity/MainActivityGame.java
+++ app/src/main/java/com/example/countingdowngame/mainActivity/MainActivityGame.java
@@ -376,7 +376,7 @@ public class MainActivityGame extends SharedMainActivity {
 
             handler.postDelayed(() -> {
                 if (chance < 10) {
-                    showDialog(currentPlayer.getName() + " is a scientist and his turn was skipped.");
+                    showDialog(currentPlayer.getName() + " is a scientist and his turn was skipped. ");
                     currentPlayer.useSkip();
                 }
             }, delayMillis);
