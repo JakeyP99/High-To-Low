8142ac8cd6dfee2e05b1f9cbb0d6632b1fd53f97
Jake Puglia <jpuglia99@hotmail.com>
2023-10-11 19:27:33 +0800

    Updated CharacterClassDescriptions

--- app/src/main/java/com/example/countingdowngame/createPlayer/CharacterClassDescriptions.java
+++ app/src/main/java/com/example/countingdowngame/createPlayer/CharacterClassDescriptions.java
@@ -1,9 +1,9 @@
 package com.example.countingdowngame.createPlayer;
 
 public class CharacterClassDescriptions {
-    public static final String CLASS_ARCHER_DESCRIPTION = "Active Ability: You can take two drinks off the total amount, and hand them out to players. \n\nPassive Ability: For every third turn, there is a 60% chance the drinking number will increase by 2 and a 40% chance it will decrease by 2.";
+    public static final String CLASS_ARCHER_DESCRIPTION = "Active Ability: You can take two drinks off the total amount, and hand them out to players. You have to repeat your turn! \n\nPassive Ability: For every third turn, there is a 60% chance the drinking number will increase by 2 and a 40% chance it will decrease by 2.";
     public static final String CLASS_WITCH_DESCRIPTION = "Active Ability: You can skip your turn. \n\nPassive Ability: For every even number your player lands on, you can hand out two drinks, but for every odd number you have to take a drink.";
-    public static final String CLASS_SCIENTIST_DESCRIPTION = "Active Ability: On your turn, you can change the current number to be whatever you want. You must roll again! \n\nPassive Ability: There's a 10% chance your turn is skipped.";
-    public static final String CLASS_SOLDIER_DESCRIPTION = "Active Ability: If the number is equal to or below 10, you can increase the drinking number by 4 drinks! Beware, you will have two turns after using this ability. \n\nPassive Ability: If you land between 10 and 15, you escape the game and are safe.";
+    public static final String CLASS_SCIENTIST_DESCRIPTION = "Active Ability: On your turn, you can change the current number to be whatever you want. You have to repeat your turn! \n\nPassive Ability: There's a 10% chance your turn is skipped.";
+    public static final String CLASS_SOLDIER_DESCRIPTION = "Active Ability: If the number is equal to or below 10, you can increase the drinking number by 4 drinks! Beware, you will have two go twice after using this ability. \n\nPassive Ability: If you land between 10 and 15, you escape the game and are safe.";
     public static final String CLASS_JIM_DESCRIPTION = "Passive Ability: For every third turn, you gain one wildcard.";
 }
