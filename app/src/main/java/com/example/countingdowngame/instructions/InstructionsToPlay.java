a1f1476d0a34995e4767b9cf29d56cb118c8121c
Jake Puglia <jpuglia99@hotmail.com>
2024-05-02 19:26:01 +0800

    Fixed the sound audio issue

--- app/src/main/java/com/example/countingdowngame/instructions/InstructionsToPlay.java
+++ app/src/main/java/com/example/countingdowngame/instructions/InstructionsToPlay.java
@@ -59,8 +59,6 @@ public class InstructionsToPlay extends ButtonUtilsActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AudioManager.getInstance().playRandomBackgroundMusic(this);
-
         setLayout();
         setupAudioManagerForMuteButtons(muteGif, soundGif);
         setupButtonControls();
