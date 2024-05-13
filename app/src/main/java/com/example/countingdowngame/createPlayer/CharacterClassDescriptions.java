bb33916412db6f83763b636b6b6a98dbe67d75dd
Jake Puglia <jpuglia99@hotmail.com>
2024-05-13 13:50:56 +0800

    Editing the view to be more aesthetically pleasing

--- app/src/main/java/com/example/countingdowngame/createPlayer/CharacterClassDescriptions.java
+++ app/src/main/java/com/example/countingdowngame/createPlayer/CharacterClassDescriptions.java
@@ -7,7 +7,7 @@ public class CharacterClassDescriptions {
     public static final String scientistActiveDescription = "You can change the current number to be whatever you want. You have to repeat your turn!";
     public static final String soldierActiveDescription = "If the number less than or equal to 10, add 4 drinks to the counter, but you must generate the number twice.";
     public static final String quizMagicianActiveDescription = "Remove one wildcard from all players.";
-    public static final String survivorActiveDescription = "Half the current number, and continue your turn. The ability resets after three turns.";
+    public static final String survivorActiveDescription = "Half the current number, and continue your turn. Ability resets after three turns.";
     public static final String jimActiveDescription = "";
     public static final String noClassDescription = "Choose this if you don't want to have any abilities.";
 
@@ -18,6 +18,6 @@ public class CharacterClassDescriptions {
     public static final String scientistPassiveDescription = "There's a 10% chance your turn is skipped.";
     public static final String soldierPassiveDescription = "If you land between 10 and 15, you escape the game. This means you no longer will be playing, and won't have to drink anymore.";
     public static final String quizMagicianPassiveDescription = "All multi-choice quizzes now have only two answers available, instead of four.";
-    public static final String survivorPassiveDescription = "If the current number is 1, and you successfully endure the generated number, you can distribute the current count of drinks. The drink number is not affected.";
+    public static final String survivorPassiveDescription = "If the current number is 1, and you generate another 1, you can distribute the current count of drinks without reducing the drink number.";
     public static final String jimPassiveDescription = "For every third turn, you gain one wildcard.";
 }
