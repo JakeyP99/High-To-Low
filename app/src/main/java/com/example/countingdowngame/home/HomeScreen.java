7dd53e95d27ee2dbc513ebe2a9ace36c5944e598
JakeyP99 <58218408+JakeyP99@users.noreply.github.com>
2025-09-13 11:49:10 +0800

    Optimized home screen

--- app/src/main/java/com/example/countingdowngame/home/HomeScreen.java
+++ app/src/main/java/com/example/countingdowngame/home/HomeScreen.java
@@ -14,15 +14,31 @@ import io.github.muddz.styleabletoast.StyleableToast;
 import pl.droidsonroids.gif.GifImageView;
 
 public class HomeScreen extends ButtonUtilsActivity {
-    private GifImageView muteGif, soundGif, drinkGif;
+
+    private GifImageView muteGif;
+    private GifImageView soundGif;
+    private GifImageView drinkGif;
     private ButtonUtils buttonUtils;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen_main_activity);
+
+        buttonUtils = new ButtonUtils(this);
+
+        initViews();
+        setupAudioManagerForMuteButtons(muteGif, soundGif);
+        setupButtons();
+        setupDrinkGif();
+    }
 
     @Override
     protected void onResume() {
         super.onResume();
-        boolean isMuted = getMuteSoundState();
-        AudioManager.getInstance().resumeBackgroundMusic();
-        AudioManager.updateMuteButton(isMuted, muteGif, soundGif);
+        AudioManager audioManager = AudioManager.getInstance();
+        audioManager.resumeBackgroundMusic();
+        AudioManager.updateMuteButton(getMuteSoundState(), muteGif, soundGif);
     }
 
     @Override
@@ -31,65 +47,58 @@ public class HomeScreen extends ButtonUtilsActivity {
         AudioManager.getInstance().pauseSound();
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_screen_main_activity);
-        buttonUtils = new ButtonUtils(this);
-
-        initializeViews();
-        setupAudioManagerForMuteButtons(muteGif, soundGif);
-        setupButtonControls();
-    }
-
-    private void initializeViews() {
+    private void initViews() {
         muteGif = findViewById(R.id.muteGif);
         soundGif = findViewById(R.id.soundGif);
         drinkGif = findViewById(R.id.drinkGif);
     }
 
+    private void setupButtons() {
+        setGameButton(R.id.quickplay, false);
+        setGameButton(R.id.playCards, true);
 
-    private void setupButtonControls() {
-        Button btnQuickPlay = findViewById(R.id.quickplay);
-        Button btnPlayCards = findViewById(R.id.playCards);
-        Button btnInstructions = findViewById(R.id.button_Instructions);
-        Button btnStatistics = findViewById(R.id.button_Statistics);
-
-        // Set onClickListener for buttons
-        btnUtils.setButton(btnQuickPlay, () -> {
-            Game.getInstance().setPlayCards(false);
-            gotoPlayerNumberChoice();
-        });
-
-        btnUtils.setButton(btnPlayCards, () -> {
-            Game.getInstance().setPlayCards(true);
-            gotoPlayerNumberChoice();
-        });
-        btnUtils.setButton(btnInstructions, this::gotoInstructions);
-        btnUtils.setButton(btnStatistics, this::gotoStatistics);
-
-        drinkGifFunctionality();
+        buttonUtils.setButton(findViewById(R.id.button_Instructions), this::gotoInstructions);
+        buttonUtils.setButton(findViewById(R.id.button_Statistics), this::gotoStatistics);
     }
 
-
-    public void drinkGifFunctionality() {
-        drinkGif.setOnClickListener(view -> {
-            AudioManager audioManager = AudioManager.getInstance();
-            audioManager.playNextSong();
-        });
-
-        drinkGif.setOnLongClickListener(view -> {
-            GeneralSettingsLocalStore settingsStore = GeneralSettingsLocalStore.fromContext(this);
-            boolean regularSoundSelected = settingsStore.shouldPlayRegularSound();
-            settingsStore.setShouldPlayRegularSound(!regularSoundSelected);
-            buttonUtils.playSoundEffects();
-            buttonUtils.vibrateDevice();
-
-            String message = regularSoundSelected ? "Burp sound effects activated!" : "Bop sound effects activated!";
-            StyleableToast.makeText(this, message, R.style.newToast).show();
-
-            return true;
+    private void setGameButton(int buttonId, boolean playCards) {
+        Button button = findViewById(buttonId);
+        buttonUtils.setButton(button, () -> {
+            Game.getInstance().setPlayCards(playCards);
+            gotoPlayerNumberChoice();
         });
     }
 
+    private void setupDrinkGif() {
+        drinkGif.setOnClickListener(v -> {
+            if (!getMuteSoundState()) {
+                AudioManager.getInstance().playNextSong();
+            }
+        });
+
+        drinkGif.setOnLongClickListener(v -> {
+            if (!getMuteSoundState()) {
+                toggleSoundEffects();
+                return true;
+            }
+            return false; // no action when muted
+        });
+    }
+
+
+
+    private void toggleSoundEffects() {
+        GeneralSettingsLocalStore settings = GeneralSettingsLocalStore.fromContext(this);
+        boolean regularSound = settings.shouldPlayRegularSound();
+
+        settings.setShouldPlayRegularSound(!regularSound);
+        buttonUtils.playSoundEffects();
+        buttonUtils.vibrateDevice();
+
+        String message = regularSound
+                ? "Burp sound effects activated!"
+                : "Bop sound effects activated!";
+
+        StyleableToast.makeText(this, message, R.style.newToast).show();
+    }
 }
