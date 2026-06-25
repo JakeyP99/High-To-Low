1bbc8ec96d555faf1d130b5096b1091e767e454c
JakeyP99 <58218408+JakeyP99@users.noreply.github.com>
2026-06-25 14:52:57 +0800

    Initial commit to new game mode

--- app/src/main/java/com/example/countingdowngame/mainActivity/MainActivityNumberGenerator.java
+++ app/src/main/java/com/example/countingdowngame/mainActivity/MainActivityNumberGenerator.java
@@ -7,39 +7,105 @@ import static com.example.countingdowngame.createPlayer.CharacterClassDescriptio
 import static com.example.countingdowngame.mainActivity.classAbilities.PassiveAbilities.handleGamblerPassiveResult;
 import static com.example.countingdowngame.mainActivity.classAbilities.PassiveAbilities.handleSurvivorPassive;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.graphics.Color;
 import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
+import com.example.countingdowngame.R;
 import com.example.countingdowngame.game.Game;
 import com.example.countingdowngame.player.Player;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class MainActivityNumberGenerator {
 
     private final MainActivityGame activity;
     private final TextView numberCounterText;
+    private final RecyclerView rouletteRecyclerView;
+    private final View roulettePointer;
     private final Handler shuffleHandler;
 
     public MainActivityNumberGenerator(MainActivityGame activity, TextView numberCounterText) {
         this.activity = activity;
         this.numberCounterText = numberCounterText;
+        this.rouletteRecyclerView = activity.findViewById(R.id.rouletteRecyclerView);
+        this.roulettePointer = activity.findViewById(R.id.roulettePointer);
         this.shuffleHandler = new Handler();
     }
 
     public void startNumberShuffleAnimation() {
         int originalNumber = Game.getInstance().getCurrentNumber();
-        int targetNumber = Game.getInstance().nextNumber(); 
-        
-        final int shuffleDuration = 1500;
-        int initialShuffleInterval = originalNumber >= 1000 ? 30 : 50;
-        final Random random = new Random();
+        int targetNumber = Game.getInstance().nextNumber();
 
-        shuffleHandler.postDelayed(new ShuffleRunnable(random, originalNumber, targetNumber, shuffleDuration, initialShuffleInterval), initialShuffleInterval);
+        if (Game.getInstance().getGameMode() == Game.GameMode.CLASS_HUNT) {
+            startRouletteAnimation(originalNumber, targetNumber);
+        } else {
+            final int shuffleDuration = 1500;
+            int initialShuffleInterval = originalNumber >= 1000 ? 30 : 50;
+            final Random random = new Random();
+            shuffleHandler.postDelayed(new ShuffleRunnable(random, originalNumber, targetNumber, shuffleDuration, initialShuffleInterval), initialShuffleInterval);
+        }
+    }
+
+    private void startRouletteAnimation(int originalNumber, int targetNumber) {
+        activity.disableButtons();
+        numberCounterText.setVisibility(View.INVISIBLE);
+        rouletteRecyclerView.setVisibility(View.VISIBLE);
+        roulettePointer.setVisibility(View.VISIBLE);
+
+        List<Integer> rouletteNumbers = new ArrayList<>();
+        Random r = new Random();
+        for (int i = 0; i < 60; i++) {
+            rouletteNumbers.add(r.nextInt(originalNumber + 1));
+        }
+        // Set the winning number at position 50
+        rouletteNumbers.set(50, targetNumber);
+
+        RouletteAdapter adapter = new RouletteAdapter(rouletteNumbers, Game.getInstance().getClassNumbers());
+        LinearLayoutManager layoutManager = new LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false);
+        rouletteRecyclerView.setLayoutManager(layoutManager);
+        rouletteRecyclerView.setAdapter(adapter);
+
+        float density = activity.getResources().getDisplayMetrics().density;
+        int itemWidthPx = (int) (100 * density);
+        int containerWidth = activity.findViewById(R.id.btnGenerate).getWidth();
+        int centerOffset = containerWidth / 2;
+
+        int totalScroll = 50 * itemWidthPx + (itemWidthPx / 2) - centerOffset;
+
+        ValueAnimator animator = ValueAnimator.ofInt(0, totalScroll);
+        animator.setDuration(4000);
+        animator.setInterpolator(new DecelerateInterpolator(1.2f));
+        animator.addUpdateListener(animation -> {
+            int currentScroll = (int) animation.getAnimatedValue();
+            layoutManager.scrollToPositionWithOffset(0, -currentScroll);
+        });
+
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                new Handler().postDelayed(() -> {
+                    rouletteRecyclerView.setVisibility(View.GONE);
+                    roulettePointer.setVisibility(View.GONE);
+                    numberCounterText.setVisibility(View.VISIBLE);
+                    revealFinalNumber(targetNumber);
+                }, 1000);
+            }
+        });
+        animator.start();
     }
 
     private class ShuffleRunnable implements Runnable {
@@ -83,51 +149,53 @@ public class MainActivityNumberGenerator {
                 shuffleHandler.postDelayed(this, currentInterval);
             } else {
                 // LAST STEP: display the actual target number and finalize
-                revealFinalNumber();
+                revealFinalNumber(targetNumber);
             }
         }
+    }
 
-        private void revealFinalNumber() {
-            int previousNumber = Game.getInstance().getPreviousNumber();
+    private void revealFinalNumber(int targetNumber) {
+        int previousNumber = Game.getInstance().getPreviousNumber();
+        Player currentPlayer = Game.getInstance().getCurrentPlayer();
 
-            Game.getInstance().recordTurn(currentPlayer, targetNumber);
+        Game.getInstance().recordTurn(currentPlayer, targetNumber);
 
-            String display = MainActivityGame.getDisplayNumber(targetNumber);
-            numberCounterText.setText(display);
-            SharedMainActivity.setTextViewSizeBasedOnInt(numberCounterText, display);
-            
-            MainActivityGame.updateNumberColor(targetNumber);
+        String display = MainActivityGame.getDisplayNumber(targetNumber);
+        numberCounterText.setText(display);
+        SharedMainActivity.setTextViewSizeBasedOnInt(numberCounterText, display);
+
+        MainActivityGame.updateNumberColor(targetNumber);
 
 
-            if ((targetNumber == 1 && previousNumber <= 1) &&
-                    (SURVIVOR.equals(currentPlayer.getClassChoice()) || ANGRY_JIM.equals(currentPlayer.getClassChoice()))) {
-                handleSurvivorPassive(currentPlayer);
-            }
+        if ((targetNumber == 1 && previousNumber <= 1) &&
+                (SURVIVOR.equals(currentPlayer.getClassChoice()) || ANGRY_JIM.equals(currentPlayer.getClassChoice()))) {
+            handleSurvivorPassive(currentPlayer);
+        }
 
-            if (GAMBLER.equals(currentPlayer.getClassChoice()) || (ANGRY_JIM.equals(currentPlayer.getClassChoice()) && previousNumber < 50)) {
-                handleGamblerPassiveResult(targetNumber);
-            }
+        if (GAMBLER.equals(currentPlayer.getClassChoice()) || (ANGRY_JIM.equals(currentPlayer.getClassChoice()) && previousNumber < 50)) {
+            handleGamblerPassiveResult(targetNumber);
+        }
 
-            if (Game.getInstance().getGameMode() == Game.GameMode.CLASS_HUNT && Game.getInstance().getClassNumbers().contains(targetNumber)) {
-                activity.disableButtons(); // Lock buttons during the long animation
-                numberCounterText.setTextColor(Color.YELLOW);
+        if (Game.getInstance().getGameMode() == Game.GameMode.CLASS_HUNT && Game.getInstance().getClassNumbers().contains(targetNumber)) {
+            activity.disableButtons(); // Lock buttons during the long animation
+            numberCounterText.setTextColor(Color.YELLOW);
 
-                YoYo.with(Techniques.Pulse)
-                        .duration(1000)
-                        .repeat(4) // 5 pulses total (1s each)
-                        .playOn(numberCounterText);
-                
-                new Handler().postDelayed(() -> {
-                    activity.awardRandomClass(currentPlayer, targetNumber);
-                    activity.enableButtons(); // Re-enable after award
-                }, 5000); // Wait for 5 seconds of pulsing
+            YoYo.with(Techniques.Pulse)
+                    .duration(1000)
+                    .repeat(4) // 5 pulses total (1s each)
+                    .playOn(numberCounterText);
 
-                Game.getInstance().getClassNumbers().remove(Integer.valueOf(targetNumber)); // Only award once
-            } else {
+            new Handler().postDelayed(() -> {
+                activity.awardRandomClass(currentPlayer, targetNumber);
                 activity.renderCurrentNumber(targetNumber, activity::gotoGameEnd, numberCounterText);
-                if (targetNumber != 0) {
-                    activity.enableButtons();
-                }
+                activity.enableButtons(); // Re-enable after award
+            }, 5000); // Wait for 5 seconds of pulsing
+
+            Game.getInstance().getClassNumbers().remove(Integer.valueOf(targetNumber)); // Only award once
+        } else {
+            activity.renderCurrentNumber(targetNumber, activity::gotoGameEnd, numberCounterText);
+            if (targetNumber != 0) {
+                activity.enableButtons();
             }
         }
     }
---
+++ app/src/main/java/com/example/countingdowngame/mainActivity/RouletteAdapter.java
@@ -0,0 +1,55 @@
+package com.example.countingdowngame.mainActivity;
+
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.countingdowngame.R;
+
+import java.util.List;
+
+public class RouletteAdapter extends RecyclerView.Adapter<RouletteAdapter.VH> {
+    private final List<Integer> numbers;
+    private final List<Integer> classNumbers;
+
+    public RouletteAdapter(List<Integer> numbers, List<Integer> classNumbers) {
+        this.numbers = numbers;
+        this.classNumbers = classNumbers;
+    }
+
+    @NonNull
+    @Override
+    public VH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_roulette_number, parent, false);
+        return new VH(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VH holder, int position) {
+        int num = numbers.get(position);
+        holder.tv.setText(String.valueOf(num));
+        if (classNumbers.contains(num)) {
+            holder.tv.setTextColor(Color.YELLOW);
+        } else {
+            holder.tv.setTextColor(Color.parseColor("#021457")); // bluedark
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return numbers.size();
+    }
+
+    static class VH extends RecyclerView.ViewHolder {
+        TextView tv;
+        VH(View v) {
+            super(v);
+            tv = v.findViewById(R.id.text_number);
+        }
+    }
+}
